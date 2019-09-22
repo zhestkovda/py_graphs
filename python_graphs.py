4cae6b135cf7d524611b35f9c2b7f8583355ea2a
Dmitry Zhestkov <zhestkovda@gmail.com>
2019-09-22 20:40:59 +0500

    popular plot types in Python

---
+++ python_graphs.py
@@ -0,0 +1,92 @@
+# !pip install brewer2mpl
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings; warnings.filterwarnings(action='once')
+
+large = 22; med = 16; small = 12
+params = {'axes.titlesize': large,
+          'legend.fontsize': med,
+          'figure.figsize': (16, 10),
+          'axes.labelsize': med,
+          'axes.titlesize': med,
+          'xtick.labelsize': med,
+          'ytick.labelsize': med,
+          'figure.titlesize': large}
+plt.rcParams.update(params)
+plt.style.use('seaborn-whitegrid')
+sns.set_style("white")
+
+# Version
+print(mpl.__version__)  #> 3.0.0
+print(sns.__version__)  #> 0.9.0
+
+
+# SCTATTER PLOT ########################################################################################################
+# Import dataset
+midwest = pd.read_csv("https://raw.githubusercontent.com/selva86/datasets/master/midwest_filter.csv")
+
+# Prepare Data
+# Create as many colors as there are unique midwest['category']
+categories = np.unique(midwest['category'])
+colors = [plt.cm.tab10(i/float(len(categories)-1)) for i in range(len(categories))]
+
+# Draw Plot for Each Category
+plt.figure(figsize=(16, 10), dpi= 80, facecolor='w', edgecolor='k')
+
+for i, category in enumerate(categories):
+    plt.scatter('area', 'poptotal',
+                data=midwest.loc[midwest.category==category, :],
+                s=20, c=colors[i], label=str(category))
+
+# Decorations
+plt.gca().set(xlim=(0.0, 0.1), ylim=(0, 90000),
+              xlabel='Area', ylabel='Population')
+
+plt.xticks(fontsize=12); plt.yticks(fontsize=12)
+plt.title("Scatterplot of Midwest Area vs Population", fontsize=22)
+plt.legend(fontsize=12)
+plt.show()
+
+# correlation diagram ####################################################################################################
+# Import Dataset
+df = pd.read_csv("https://github.com/selva86/datasets/raw/master/mtcars.csv")
+
+# Plot
+plt.figure(figsize=(12,10), dpi= 80)
+sns.heatmap(df.corr(), xticklabels=df.corr().columns, yticklabels=df.corr().columns, cmap='RdYlGn', center=0, annot=True)
+
+# Decorations
+plt.title('Correlogram of mtcars', fontsize=22)
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
+plt.show()
+
+# DUAL PLOT ############################################################################################################
+# Load Dataset
+df = sns.load_dataset('iris')
+plt.clf()
+plt.cla()
+# Plot
+#plt.figure(figsize=(10,8), dpi= 80)
+sns.pairplot(df, kind="scatter", hue="species", plot_kws=dict(s=80, edgecolor="white", linewidth=2.5))
+plt.show()
+
+# PROBABILITY DENSITY ##################################################################################################
+
+# Import Data
+df = pd.read_csv("https://github.com/selva86/datasets/raw/master/mpg_ggplot2.csv")
+
+# Draw Plot
+plt.figure(figsize=(16,10), dpi= 80)
+sns.kdeplot(df.loc[df['cyl'] == 4, "cty"], shade=True, color="g", label="Cyl=4", alpha=.7)
+sns.kdeplot(df.loc[df['cyl'] == 5, "cty"], shade=True, color="deeppink", label="Cyl=5", alpha=.7)
+sns.kdeplot(df.loc[df['cyl'] == 6, "cty"], shade=True, color="dodgerblue", label="Cyl=6", alpha=.7)
+sns.kdeplot(df.loc[df['cyl'] == 8, "cty"], shade=True, color="orange", label="Cyl=8", alpha=.7)
+
+# Decoration
+plt.title('Density Plot of City Mileage by n_Cylinders', fontsize=22)
+plt.legend()
+plt.show()
